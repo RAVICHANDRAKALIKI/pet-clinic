ebc449148b23c87d0bd89da80ef1cd60189615e3
RAVICHANDRAKALIKI <k.ravichandrareddy@gmail.com>
2020-05-17 16:41:52 +0530

    Initial Commit

---
+++ src/main/java/dev/ravi/petclinic/PetClinicProjectUsingSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.ravi.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicProjectUsingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicProjectUsingSpringApplication.class, args);
+	}
+
+}
